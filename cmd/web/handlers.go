6e9b83b9f0c3d3decdde460774ca1bf15aeba237
Vanshikav123 <vanshikav928@gmail.com>
2025-01-07 23:35:16 +0530

    routes
    Signed-off-by: Vanshikav123 <vanshikav928@gmail.com>

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,13 +7,14 @@ import (
 	"strconv"
 
 	"github.com/Vanshikav123/gosnippet.git/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
+	/*if r.URL.Path != "/" {
 		app.notFound(w)
 		return
-	}
+	}*/
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -46,27 +47,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "home.html", data)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
-
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
-}
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	queryparams := r.URL.Query()
-	id, err := strconv.Atoi(queryparams.Get("id"))
+	queryparams := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(queryparams.ByName("id"))
 
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -104,4 +87,23 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	data.Snippet = snippet
 
 	app.render(w, http.StatusOK, "view.html", data)
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "create.html", data)
 }
